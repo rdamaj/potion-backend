f52b8bdac627dc32e3a85c7835378b683f61d535
rdamaj <rayd72@hotmail.com>
2025-02-15 15:28:48 -0500

    almost done

--- app/deployments/main/wallets/jobs/sync.js
+++ app/deployments/main/wallets/jobs/sync.js
@@ -3,6 +3,8 @@ import { getApp, parameterTypes } from "../../../../helpers/api.js";
 import { WALLETS } from "../../../../helpers/constants.js";
 import HeliusClient from "../../../../helpers/data/helius-client.js";
 import { parseTrades, debug } from "../../../../helpers/data/pnl.js";
+import { upsertMany } from "../../../../helpers/data/postgres-helper.js";
+
 const config = {
     type: parameterTypes.none,
     unknownParameters: true,
@@ -11,11 +13,24 @@ const config = {
 
 const handler = getApp(async () => {
     const THIRTY_DAYS_AGO = Math.floor(Date.now() / 1000) - (30 * 24 * 60 * 60);
-    const trades = await HeliusClient.getAllWalletTrades('BmzsfqD2PSmEyAMwPiF3hVVeZ39g6pg39GmizVsHaBhk', THIRTY_DAYS_AGO);
-    // await debug(trades);
     const solPrice = await HeliusClient.getSolPrice();
-    const tradedTokens = await parseTrades(trades, 'BmzsfqD2PSmEyAMwPiF3hVVeZ39g6pg39GmizVsHaBhk', solPrice);
-    console.log(tradedTokens);
+    for (const wallet of WALLETS.slice(20,30)) {
+        try{
+            const trades = await HeliusClient.getAllWalletTrades(wallet, THIRTY_DAYS_AGO);
+            const tradedTokens = await parseTrades(trades, wallet, solPrice);
+            const tradesArray = Object.values(tradedTokens);
+            if(tradesArray.length > 0){
+                let result = await upsertMany('trades', tradesArray, ['id']);
+                if(result){
+                    console.log(`Synced ${tradesArray.length} trades for wallet ${wallet}`);
+                }
+            } else {
+                console.log(`No trades found for wallet ${wallet}`);
+            }
+        } catch(e){
+            console.error(`Error syncing wallet ${wallet}: ${e}`);
+        }
+    }
 }, config);
 
 export { handler };
